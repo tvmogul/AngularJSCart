9f86d2a6bb17db5c36fd8bb15b34034f7fb9e841
TVMogul <tvmogul1@yahoo.com>
2015-03-28 06:17:30 -0400

    update

---
+++ AngularJSCart/ac_products/config.js
@@ -0,0 +1,39 @@
+
+//Setup each store below:
+var config_module = angular.module('storeApp.config', [])
+.constant('CONFIG', {
+    'STORE_ID': 'SerGioApps',
+    'STORE_PAGE': 'storefront.html',
+    'STORE_BG_IMAGE': 'ac_img/bg1.jpg',
+    'PRODUCTS_FILE': 'ac_products/products.txt',
+    'PRODUCTS_FOLDER': 'ac_products',
+    'NAVBAR_THEME': 'navbar_gray_gradient',
+    'NAVBAR_LOGO_TEXT': 'Your Logo',
+    'NAVBAR_LOGO_LINK': 'http://www.CodeProject.com/',
+    'INSIDE_HEADER_SHOW': true,
+    'INSIDE_HEADER_LINK': 'http://www.CodeProject.com/',
+    'INSIDE_HEADER_IMAGE': 'ac_img/logo.png',
+    'INSIDE_HEADER_TITLE': 'Responsive Angular Cart',
+    'CAROUSEL_SHOW': false,
+    'SYSTEM_NAME': 'My Angular JS project',
+    'SYSTEM_LANGUAGE': '',
+    'BASE_URL': '',
+    'API_URL': '',
+    'GOOGLE_ANALYTICS_ID': '',
+    'DB': null,
+    'DATABASENAME': 'serviceapp',
+    'TABLE1': 'serviceorders',
+    'TABLE2': 'photos',
+    'KEYPATH1': 'primarykey',
+    'KEYPATH2': 'data',
+    'INDEX1': 'needsynchronization',
+    'INDEX2': 'ordernumber',
+    'INDEX3': 'timestamp',
+    'DB_VERSION': 1,
+    'GLOBALCOUNTER': 0,
+    'LOADED': false,
+    'SERVICEORDERS': {}
+});
+
+
+

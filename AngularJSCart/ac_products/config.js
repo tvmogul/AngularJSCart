259f606701bad834a98d52555dfa1b51dfd89c4e
Bill SerGio, The Infomercial King <tvmogul1@yahoo.com>
2016-03-01 09:40:22 -0500

    Added files via upload

--- AngularJSCart/ac_products/config.js
+++ AngularJSCart/ac_products/config.js
@@ -1,8 +1,17 @@
 
 //Setup each store below:
+// Store can be loadded from a JSON file, a local .mdf database, or a remote database.
+//'CF_DATA_SOURCE': 'ac_products/products.js',
+//'CF_DATA_SOURCE': '/crud.ashx?ac=getproducts&cn=local',
+//'CF_DATA_SOURCE': '/crud.ashx?ac=getproducts&cn=remote',
 var config_module = angular.module('storeApp.config', [])
 .constant('CONFIG',
 {
+    'CF_DATA_FILE': 'ac_products/products.js',
+    'CF_DATA_LOCAL': '/crud.ashx?ac=getproducts&cn=local',
+    'CF_DATA_REMOTE': '/crud.ashx?ac=getproducts&cn=remote',
+    'CF_PRODUCTS_FILE': 'ac_products/products.js',
+    'CF_PRODUCTS_FOLDER': 'ac_products',
     'CF_STORE_ID': 'SerGioApps',
     'CF_STORE_PAGE': 'storefront.html',
     'CF_STORE_BG_IMAGE': 'ac_img/bg1.jpg',
@@ -10,16 +19,15 @@ var config_module = angular.module('storeApp.config', [])
     'CF_PAYMENT_PAYPAL_BUYNOW': 'paypaluser@youremail.com',
     'CF_PAYMENT_GOOGLE_WALLET_ID': 'GooGle_Wallet_ID',
     'CF_PAYMENT_STRIPE': 'pk_test_stripe',
-    'CF_PRODUCTS_FILE': 'ac_products/products.txt',
-    'CF_PRODUCTS_FOLDER': 'ac_products',
-    'CF_NAVBAR_THEME': 'navbar_gray_gradient',
-    'CF_NAVBAR_LOGO_TEXT': 'SerGioApps',
-    'CF_NAVBAR_LOGO_LINK': 'http://www.Software-rus.com/',
+    'CF_NAVBAR_THEME': 'navbar_dkred_gradient',
+    'CF_NAVBAR_LOGO_IMAGE': 'ac_img/logo.png',
+    'CF_NAVBAR_LOGO_TEXT': '',
+    'CF_NAVBAR_LOGO_LINK': 'https://www.youtube.com/watch?v=HppJHKwCGCo',
     'CF_INSIDE_HEADER_SHOW': true,
-    'CF_INSIDE_HEADER_LINK': 'http://www.Software-rus.com/',
+    'CF_INSIDE_HEADER_LINK': 'https://www.youtube.com/watch?v=HppJHKwCGCo',
     'CF_INSIDE_HEADER_IMAGE': 'ac_img/logo.png',
     'CF_INSIDE_HEADER_TITLE': 'Responsive Angular Cart',
-    'CF_CAROUSEL_SHOW': false,
+    'CF_CAROUSEL_SHOW': true,
     'CF_CAROUSEL_AUTO_PLAY': true,
     'CF_AN_CAROUSEL_IMG_VIDEO': 'hvr-pulse-grow',
     'CF_AN_CAROUSEL_PILL': 'hvr-wobble-to-top-right',
@@ -29,22 +37,8 @@ var config_module = angular.module('storeApp.config', [])
     'CF_SYSTEM_LANGUAGE': '',
     'CF_BASE_URL': '',
     'CF_API_URL': '',
-    'CF_GOOGLE_ANALYTICS_ID': '',
-    'CF_DB': null,
-    'CF_DATABASENAME': 'serviceapp',
-    'CF_TABLE1': 'serviceorders',
-    'CF_TABLE2': 'photos',
-    'CF_KEYPATH1': 'primarykey',
-    'CF_KEYPATH2': 'data',
-    'CF_INDEX1': 'needsynchronization',
-    'CF_INDEX2': 'ordernumber',
-    'CF_INDEX3': 'timestamp',
-    'CF_DB_VERSION': 1,
-    'CF_GLOBALCOUNTER': 0,
-    'CF_LOADED': false,
-    'CF_ADDRESS': [' 3156 Dusty Highway', ' Teaneck New Jersey 07009-6370 US']
-}
-);
+    'CF_GOOGLE_ANALYTICS_ID': ''
+});
 
 
 
